38c7faf63cbcf2fb8a55e90071d6933ecf6ece3e
Anup-D-Dutta <codewithanup@gmail.com>
2025-03-12 15:09:44 +0530

    fixed

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,21 +1,46 @@
+// import mongoose from "mongoose";
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+// if(!process.env.MONGODB_URI){
+//     throw new Error(
+//         "Please provide MONGODB_URI in the .env file"
+//     )
+// }
+
+// async function connectDB(){
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URI)
+//         console.log("connect DB")
+//     } catch (error) {
+//         console.log("Mongodb connect error",error)
+//         process.exit(1)
+//     }
+// }
+
+// export default connectDB
+
+
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
 
-if(!process.env.MONGODB_URI){
-    throw new Error(
-        "Please provide MONGODB_URI in the .env file"
-    )
-}
+dotenv.config();
 
-async function connectDB(){
-    try {
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log("connect DB")
-    } catch (error) {
-        console.log("Mongodb connect error",error)
-        process.exit(1)
-    }
-}
+const connectDB = async () => {
+  try {
+    console.log("Connecting to MongoDB...");
+    console.log("MONGO_URI:", process.env.MONGODB_URI ? "Loaded" : "Not Loaded");
 
-export default connectDB
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
