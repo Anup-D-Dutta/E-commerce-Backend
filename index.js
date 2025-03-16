509624a77ae991ef150d3a728459d50fd030b24d
Anup-D-Dutta <codewithanup@gmail.com>
2025-03-16 11:41:55 +0530

    fixed

--- index.js
+++ index.js
@@ -28,14 +28,9 @@ const allowedOrigins = [
 
 app.use(cors({
     credentials: true,
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
+    origin: "*",
 }));
+
 
 app.use(express.json())
 app.use(cookieParser())
